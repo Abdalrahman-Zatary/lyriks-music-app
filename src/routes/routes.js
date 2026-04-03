e331d30550a5c0f4a312a2fb0cf2650cae118ebe
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-03 21:32:53 +0300

    feat: Add routes for AroundYou, ArtistDetails, Discover, Search, SongDetails, TopArtists, and TopCharts components

---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+import AroundYou from './around-you/around-you.component';
+import ArtistDetails from './artist-details/artist-details.component';
+import Discover from './discover/discover.component';
+import Search from './search/search.component';
+import SongDetails from './song-details/song-details.component';
+import TopArtists from './top-artists/top-artists.component';
+import TopCharts from './top-charts/top-charts.component';
+
+export {
+  AroundYou,
+  ArtistDetails,
+  Discover,
+  Search,
+  SongDetails,
+  TopArtists,
+  TopCharts,
+};
+
