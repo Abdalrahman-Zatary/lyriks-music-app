024a7c32acbb8fa9a63e8dd84240e81409292ff1
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-03 21:54:48 +0300

    feat: create and export common UI components for reusability

---
+++ src/components/components.js
@@ -0,0 +1,25 @@
+import Sidebar from './sidbar/sidebar.component';
+import Searchbar from './searchbar/searchbar.component';
+import SongCard from './song-card/song-card.component';
+import TopPlay from './top-play/top-play.component';
+import ArtistCard from './artist-card/artist-card.component';
+import DetailsHeader from './details-header/details-header.component';
+import SongBar from './songbar/songbar.component';
+import RelatedSongs from './related-songs/related-songs.component';
+import MusicPlayer from './MusicPlayer';
+import Loader from './loader/loader.component';
+import Error from './error/error.component';
+
+export {
+  TopPlay,
+  Sidebar,
+  SongCard,
+  Searchbar,
+  ArtistCard,
+  DetailsHeader,
+  SongBar,
+  RelatedSongs,
+  MusicPlayer,
+  Loader,
+  Error,
+};
