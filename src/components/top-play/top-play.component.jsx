b77a12b3b5e0f72ed2edc551ef48345bc6e720bf
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-06 01:30:42 +0300

    feat: add TopPlay component and Swiper

---
+++ src/components/top-play/top-play.component.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import TopChartCard from '../top-chart-card/top-chart-card.component';
+import { playPause, setActiveSong } from '../../redux/features/playerSlice';
+import { useGetTopTracksQuery } from '../../redux/services/deezerApi';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopTracksQuery();
+  const divRef = useRef(null);
+
+  // console.log(data);
+
+  const topPlays = data?.slice(0, 5);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song: topPlays[0], data, i: 0 }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+              key={song.id}
+              song={song}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+              i={i}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays?.map((song) => (
+            <SwiperSlide
+              key={song.id}
+              style={{ width: '25%', height: '25%' }}
+              className="rounded-full w-full animate-slideright"
+            >
+              <Link to={`/artists/${song?.artist?.name}`}>
+                <img
+                  src={song.artist.picture}
+                  alt="name"
+                  className="rounded-full w-full object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default TopPlay;
