5369acbc00199deb3c8dbda89e223f30f9342057
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-04-08 23:22:55 +0300

    feat: add Searchbar component for song search functionality

---
+++ src/components/searchbar/searchbar.component.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      autoCapitalize="off"
+      className="h-[55px] flex items-center text-gray-400 focus-within:text-gray-600"
+    >
+      <label htmlFor="search-field" className="sr-only">
+        Search all songs
+      </label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch className="w-5 h-5 mr-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white text-left"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Searchbar;
